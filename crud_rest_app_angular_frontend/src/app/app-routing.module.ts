6cc22d33675dbdd2f9011c3708109af0fac7dfc2
ratlord <bennett.daniel@gmail.com>
2022-07-30 21:33:13 -0400

    added routing

--- crud_rest_app_angular_frontend/src/app/app-routing.module.ts
+++ crud_rest_app_angular_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { LandingComponent } from './views/landing/landing.component';
+import { LoginComponent } from './views/login/login.component';
+import { MainComponent } from './views/main/main.component';
+import { RegisterComponent } from './views/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'workout', component: MainComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
